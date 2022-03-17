87ee05ee05d8a18fe791f78c0f0798f2b8a7710c
Carb2750 <christianrapalo@hotmail.com>
2022-03-17 16:35:32 -0600

    Add typescript object export

---
+++ build/ts/tokens.ts
@@ -0,0 +1,58 @@
+export const tokens = {
+  "iconicColorActionPrimary100": "#75b8ddff",
+  "iconicColorActionPrimary200": "#60a1c5ff",
+  "iconicColorActionPrimary300": "#4d8eb2ff",
+  "iconicColorActionPrimary400": "#337499ff",
+  "sm": {
+    "fontSize": 12,
+    "textDecoration": "none",
+    "fontFamily": "Roboto",
+    "fontWeight": 400,
+    "fontStyle": "normal",
+    "fontStretch": "normal",
+    "letterSpacing": 0,
+    "lineHeight": 14.4,
+    "paragraphIndent": 0,
+    "paragraphSpacing": 0,
+    "textCase": "none"
+  },
+  "md": {
+    "fontSize": 14,
+    "textDecoration": "none",
+    "fontFamily": "Roboto",
+    "fontWeight": 400,
+    "fontStyle": "normal",
+    "fontStretch": "normal",
+    "letterSpacing": 0,
+    "lineHeight": 16.8,
+    "paragraphIndent": 0,
+    "paragraphSpacing": 0,
+    "textCase": "none"
+  },
+  "lg": {
+    "fontSize": 18,
+    "textDecoration": "none",
+    "fontFamily": "Roboto",
+    "fontWeight": 400,
+    "fontStyle": "normal",
+    "fontStretch": "normal",
+    "letterSpacing": 0,
+    "lineHeight": 21.6,
+    "paragraphIndent": 0,
+    "paragraphSpacing": 0,
+    "textCase": "none"
+  },
+  "xl": {
+    "fontSize": 24,
+    "textDecoration": "none",
+    "fontFamily": "Roboto",
+    "fontWeight": 400,
+    "fontStyle": "normal",
+    "fontStretch": "normal",
+    "letterSpacing": 0,
+    "lineHeight": 28.8,
+    "paragraphIndent": 0,
+    "paragraphSpacing": 0,
+    "textCase": "none"
+  }
+};
--- transformTokens.js
+++ transformTokens.js
@@ -1,59 +1,78 @@
-const StyleDictionary = require('style-dictionary')
-const baseConfig = require('./config.json')
+const StyleDictionary = require("style-dictionary");
+const baseConfig = require("./config.json");
+
+const { minifyDictionary, fileHeader } = StyleDictionary.formatHelpers;
 
 StyleDictionary.registerTransform({
-  name: 'size/px',
-  type: 'value',
-  matcher: token => {
-    return (token.unit === 'pixel' || token.type === 'dimension') && token.value !== 0
+  name: "size/px",
+  type: "value",
+  matcher: (token) => {
+    return (
+      (token.unit === "pixel" || token.type === "dimension") &&
+      token.value !== 0
+    );
   },
-  transformer: token => {
-    return `${token.value}px`
-  }
-})
+  transformer: (token) => {
+    return `${token.value}px`;
+  },
+});
 
 StyleDictionary.registerTransform({
-  name: 'size/percent',
-  type: 'value',
-  matcher: token => {
-    return token.unit === 'percent' && token.value !== 0
+  name: "size/percent",
+  type: "value",
+  matcher: (token) => {
+    return token.unit === "percent" && token.value !== 0;
   },
-  transformer: token => {
-    return `${token.value}%`
-  }
-})
+  transformer: (token) => {
+    return `${token.value}%`;
+  },
+});
 
 StyleDictionary.registerTransformGroup({
-  name: 'custom/css',
-  transforms: StyleDictionary.transformGroup['css'].concat([
-    'size/px',
-    'size/percent'
-  ])
-})
+  name: "custom/css",
+  transforms: StyleDictionary.transformGroup["css"].concat([
+    "size/px",
+    "size/percent",
+  ]),
+});
 
 StyleDictionary.registerTransformGroup({
-  name: 'custom/less',
-  transforms: StyleDictionary.transformGroup['less'].concat([
-    'size/px',
-    'size/percent'
-  ])
-})
+  name: "custom/less",
+  transforms: StyleDictionary.transformGroup["less"].concat([
+    "size/px",
+    "size/percent",
+  ]),
+});
 
 StyleDictionary.registerTransformGroup({
-  name: 'custom/scss',
-  transforms: StyleDictionary.transformGroup['less'].concat([
-    'size/px',
-    'size/percent'
-  ])
-})
+  name: "custom/scss",
+  transforms: StyleDictionary.transformGroup["less"].concat([
+    "size/px",
+    "size/percent",
+  ]),
+});
 
 StyleDictionary.registerFilter({
-  name: 'validToken',
-  matcher: function(token) {
-    return ['dimension', 'string', 'number', 'color'].includes(token.type)
-  }
-})
+  name: "validToken",
+  matcher: function (token) {
+    return ["dimension", "string", "number", "color"].includes(token.type);
+  },
+});
 
-const StyleDictionaryExtended = StyleDictionary.extend(baseConfig)
+StyleDictionary.registerFormat({
+  name: "typescript/object",
+  formatter: function ({ dictionary, file }) {
+    return (
+      fileHeader({ file }) +
+      "export const " +
+      (file.name || "tokens") +
+      " = " +
+      JSON.stringify(minifyDictionary(dictionary.tokens), null, 2) +
+      ";"
+    );
+  },
+});
 
-StyleDictionaryExtended.buildAllPlatforms()
+const StyleDictionaryExtended = StyleDictionary.extend(baseConfig);
+
+StyleDictionaryExtended.buildAllPlatforms();
